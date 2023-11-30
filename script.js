3976d126a539ac3ea2676881fdf41d96bd48945e
bnasare <124492150+bnasare@users.noreply.github.com>
2023-11-30 19:37:26 +0000

    updates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bc5f14f026b71687afbff6cc7bad4d4b&page=1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bc5f14f026b71687afbff6cc7bad4d4b&page=2';
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
